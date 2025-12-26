63e68c35da61b350c70e67e74c2e811adc154735
Sajina Dhakal <saznadha@gmail.com>
2025-12-26 21:06:33 +0545

    Add root URL view to fix 404 error

--- trekking_app/urls.py
+++ trekking_app/urls.py
@@ -1,12 +1,17 @@
-"""
+﻿"""
 URL configuration for trekking_app project.
 """
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to my Django project!")
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
